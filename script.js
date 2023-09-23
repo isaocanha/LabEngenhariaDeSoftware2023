813648a7b1f0f08a050897f01d2dcc43ad6f7bd2
Dobrido <victor.campos1410@gmail.com>
2023-09-22 23:32:54 -0300

    Tela Inicial

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('b1').addEventListener('click', function() {
+  window.location.href = 'index2.html';
+});
+
+document.getElementById('b2').addEventListener('click', function() {
+  window.location.href = 'index3.html';
+});
