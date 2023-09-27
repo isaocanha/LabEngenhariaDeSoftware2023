169744a9b1e86536804d29f07157b7b86ac5b360
Victor Campos <32129701@mackenzista.com.br>
2023-09-27 14:20:42 -0300

    Telas e calculadora pronta

--- script.js
+++ script.js
@@ -5,3 +5,24 @@ document.getElementById('b1').addEventListener('click', function() {
 document.getElementById('b2').addEventListener('click', function() {
   window.location.href = 'index3.html';
 });
+
+const valorPapel = 0.01;
+const valorPlastico = 0.02;
+const valorMetal = 0.03;
+const valorVidro = 0.04;
+
+function calcularDesconto() {
+  const papel = parseFloat(document.getElementById('papel').value) || 0;
+  const plastico = parseFloat(document.getElementById('plastico').value) || 0;
+  const metal = parseFloat(document.getElementById('metal').value) || 0;
+  const vidro = parseFloat(document.getElementById('vidro').value) || 0;
+
+  const descontoTotal = (papel * valorPapel) + (plastico * valorPlastico) + (metal * valorMetal) + (vidro * valorVidro);
+  
+  document.getElementById('resultado').innerText = descontoTotal.toFixed(2);
+}
+
+document.getElementById('papel').addEventListener('input', calcularDesconto);
+document.getElementById('plastico').addEventListener('input', calcularDesconto);
+document.getElementById('metal').addEventListener('input', calcularDesconto);
+document.getElementById('vidro').addEventListener('input', calcularDesconto);
